cf615dcb52ad9a4600482704ba230e284fb7fce9
johnocamposabg-dev <john.ocamposabg@gmail.com>
2026-01-08 00:17:59 +0000

    avance 1/3

--- src/app.py
+++ src/app.py
@@ -6,11 +6,14 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
-
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 # from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
@@ -18,6 +21,8 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../dist/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -65,6 +70,19 @@ def serve_any_other_file(path):
     response.cache_control.max_age = 0  # avoid cache memory
     return response
 
+@app.route('/login', method=['POST'])
+def login():
+    body= request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg':"Email and Password is needed"}),400
+    if email not in body:
+        return
+
+@app.route("/private", methods=["GET"])
+@jwt_required()
+def private():
+    user = get_jwt_identity()
+    return jsonify(logged_in_as=user), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
